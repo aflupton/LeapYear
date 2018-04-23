dd68c228fc1fe52763a4b260ff426317017f8bab
Andrew Lupton <aflupton@gmail.com>
2018-04-23 09:47:37 -0700

    add tests tests 2 and 3

--- LeapYear.Tests/ModelTests/LeapYearTest.cs
+++ LeapYear.Tests/ModelTests/LeapYearTest.cs
@@ -9,7 +9,26 @@ namespace LeapYear.Tests
     [TestMethod]
     public void IsLeapYear_NumberDivisibleByFour_True()
     {
-      //code goes here
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+    }
+    [TestMethod]
+    public void IsLeapYear_NumberNotDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
+    }
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+    }
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfFourHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
     }
   }
 }
--- LeapYear/Models/LeapYear.cs
+++ LeapYear/Models/LeapYear.cs
@@ -7,8 +7,18 @@ namespace LeapYear
   {
     public bool IsLeapYear(int year)
     {
-      //code goes here
-      return false;
+      if (year % 400 == 0)
+      {
+        return true;
+      }
+      else if (year % 100 == 0)
+      {
+        return false;
+      }
+      else
+      {
+        return year % 4 == 0;
+      }
     }
   }
 }
